e9517d2c28158e3dee5f43105846f47914f33a36
ankur <ankur.pata@gmail.com>
2024-01-22 10:01:04 +0400

    fix(config): Remove unwanted config from .env and validations

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -63,7 +63,7 @@ const sendVerificationEmailByToken = async (to, token) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
   // const verificationEmailUrl = `http://localhost:10001/v1/auth/verify-email?token=${token}`;
-  const verificationEmailUrl = `${process.env.DOMAIN_URL}/verify-email?token=${token}`;
+  const verificationEmailUrl = `${process.env.EMAIL_VERIFY_URL}?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
