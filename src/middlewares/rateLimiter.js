f58e816b73c3cd856ed859d428edf5aa90784a4f
Ankur Pata <ankur@sling.biz>
2024-12-13 10:21:25 +0400

    chore: change max request in 15 min to 50

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20, // Limit each IP to 20 requests per windowMs
+  max: 50, // Limit each IP to 20 requests per windowMs
   skipSuccessfulRequests: false, // Count successful requests against the rate limit
   message: 'Too many requests from this IP, please try again later',
 });
